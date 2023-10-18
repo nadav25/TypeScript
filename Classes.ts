54d43b60216bf2e10844a67e53c87b999a467c26
nadav <32885396+nadav25@users.noreply.github.com>
2023-10-19 02:36:56 +0300

    Update Classes.ts

--- Classes.ts
+++ Classes.ts
@@ -1,3 +1,24 @@
-function() {
+
+class Employee {
+    id: number
+    address: string;
+
+
+    constructor( id : number ,address : string){
+        this.id = id;
+        this.address = address;
+    } 
+
+   getNameWithAddres():string{
+        return `${this.name} stays at ${this.address}`
+    }
+
   
+
 }
+
+let john = new Employee(10,"maza2");
+
+console.log(john);
+console.log(john.getNameWithAddres());
+
